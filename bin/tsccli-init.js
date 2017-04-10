3ad3598203ecc726522471f1310eaf0f78e5da7e
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-04-09 23:04:07 -0500

    code update

--- bin/tsccli-init.js
+++ bin/tsccli-init.js
@@ -6,6 +6,7 @@ var config = require('../config.json');
 var prompt = require('co-prompt');
 var program = require('commander');
 var fs = require('fs');
+const username = require('username');
 
 var logoutpout = 'c:\\tsccli_output.log';
 
@@ -13,21 +14,22 @@ if (!shell.which('git')) {
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
 }
-var doCreateProject = (name) => {
-    shell.echo(`Creating project ${name}`);
-    shell.exec(`git clone ${config.source_repo} ${name}`);
+var doCreateProject = (projectName, authorName) => {
+    shell.echo(`Creating project ${projectName}`);
+    shell.exec(`git clone ${config.source_repo} ${projectName}`);
     shell.echo(`Project created. Preparing...`);
-    shell.cd(name);
+    shell.cd(projectName);
     shell.exec(`rmdir .git /s /q`, {
         silent: true
     });
     var packageJsonString = shell.cat(`package.json`);
     var projectJson = JSON.parse(packageJsonString);
-    projectJson.name = name;
+    projectJson.name = projectName;
+    projectJson.author = authorName;
     fs.writeFile(`./package.json`, JSON.stringify(projectJson), function (err) {
         if (err) {
             console.log(err);
-         return  shell.echo('Unable to update project config');
+            return shell.echo('Unable to update project config');
         }
         shell.echo("Project config updated");
         shell.echo(`Installing dependencies...`);
@@ -38,9 +40,29 @@ var doCreateProject = (name) => {
     });
 
 }
-
+var getUserInput = (tempName, callback) => {
+    co(function* () {
+        var authorName = yield prompt(`Author (${tempName}): `);
+        callback(authorName);
+    });
+}
 var processAction = (projectName) => {
-    doCreateProject(projectName);
+    username().then(username => {
+        getUserInput(username, (authorName) => {
+            if (!authorName || authorName.length < 1) {
+                authorName = username;
+            }
+            doCreateProject(projectName, authorName);
+        });
+    }).catch(err => {
+        getUserInput("<none>", (authorName) => {
+            if (!authorName || authorName.length < 1) {
+                authorName = "";
+            }
+            doCreateProject(projectName, authorName);
+        });
+    });
+
 }
 program
     .usage('Creates a new project with the supplied project name')
